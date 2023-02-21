ee45997d9e200c410345682a621ea6ad9877d936
csurbeck <csurbeck@uw.edu>
2023-02-21 09:53:59 -0800

    finished data visualizations

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-d3.csv("data/museumIncome.csv")
-  .then(plotMuseumData)
+var currentChart;
 
+window.addEventListener('load', init);
 
-function plotMuseumData(states) {
+function init() {
+  incomeData();
+  id("count").addEventListener("click", countData);
+  id("income").addEventListener("click", clickIncomeData);
+}
+
+function incomeData() {
+  d3.csv("data/museumIncome.csv")
+  .then(plotIncomeData)
+}
+
+function clickIncomeData() {
+  currentChart.destroy();
+  incomeData();
+}
+
+function countData() {
+  currentChart.destroy();
+  d3.csv("data/museumCount.csv")
+  .then(plotCountData)
+}
+
+function plotIncomeData(states) {
   var stateLabels = states.map(function(d) {return d.State});
   var incomeData = states.map(function(d) {return d.AverageIncome});
 
-  var chart = new Chart('chart', {
+  currentChart = new Chart('chart', {
     type: 'bar',
     data: {
       labels: stateLabels,
       datasets: [
         {
+          label: "Average Income",
           data: incomeData
         }
       ]
@@ -20,4 +43,35 @@ function plotMuseumData(states) {
       indexAxis: 'y',
     }
   });
+}
+
+function plotCountData(states) {
+  var stateLabels = states.map(function(d) {return d.State});
+  var countData = states.map(function(d) {return d.Count});
+
+  currentChart = new Chart('chart', {
+    type: 'bar',
+    data: {
+      labels: stateLabels,
+      datasets: [
+        {
+          label: "Number of Museums",
+          data: countData
+        }
+      ]
+    },
+    options: {
+      indexAxis: 'y',
+    }
+  });
+}
+
+
+/**
+   * retrieves an HTML element by ID
+   * @param {String} id - the id tag for an element in the HTML
+   * @returns {Node} - the element from the HTML document
+   */
+function id(id) {
+  return document.getElementById(id);
 }
